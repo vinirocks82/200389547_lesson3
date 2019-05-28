b9ad8fedc4ff0a876438892a94959dea6a4abf2d
vinirocks82 <50341542+vinirocks82@users.noreply.github.com>
2019-05-28 17:53:51 -0400

    Add files via upload

---
+++ Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200389547.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+    }
+}
